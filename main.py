07a690ceece71b5c23d89122125c66f24cb9697c
guy <guyblau100@gmail.com>
2023-12-18 16:17:37 +0200

    typo fix and updated api routes.

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ import DB_Management
 app = Flask(__name__)
 
 
-
-@app.route('/userTests', methods=['GET'])
+@app.route('/getUserAvailableTests', methods=['GET'])
 def get_user_tests():
-    userEmail = request.json['email']
+    userEmail = request.json['user_email']
     testYear = request.json['test_year']
     try:
         tests = DB_Management.getUserAvailableTestsToDo(userEmail,testYear)
@@ -18,46 +17,64 @@ def get_user_tests():
         return jsonify({'error': {'code': 400, 'message': str(e)}}), 400
 
 
-@app.route('/test/getFirstQuestions', methods=['GET'])
+@app.route('/getTestFirstQuestions', methods=['GET'])
 def get_test_first_questions():
-    testSeasonMonth = request.json['testSeasonMonth']
-    testYear = request.json['testYear']
+    testSeasonMonth = request.json['test_season_or_month']
+    testYear = request.json['test_year']
     questions = DB_Management.getSimulationSectionsFirstQuestions(testYear, testSeasonMonth)
     return jsonify(questions), 200
 
-@app.route('/user/<string:userEmail>', methods=['GET'])
-def get_graph_page_data(userEmail):
+
+@app.route('/getUserGraphs', methods=['GET'])
+def get_graph_page_data():
+    userEmail = request.json['user_email']
     try:
         data = DB_Management.getStatisticsPageData(userEmail)
-        return jsonify(data), 200
+        return jsonify(data)
     except Exception as e:
         return jsonify({'error': {'code': 400, 'message': str(e)}}), 400
 
 
-@app.route('/createTest', methods=['POST'])
+@app.route('/getUserTests', methods=['GET'])
+def get_test_report():
+    userEmail = request.json['user_email']
+    examYear = request.json['test_year']
+    examSeasonOrMonth = request.json['test_season_or_month']
+    try:
+        resultJson = DB_Management.getSimulationReport(userEmail, examYear, examSeasonOrMonth)
+        return jsonify(resultJson)
+    except Exception as e:
+        return jsonify({'error': {'code': 400, 'message': str(e)}}), 400
+
+
+
+@app.route('/insertNewTest', methods=['POST'])
 def insert_test():
-    data = request.get_json()
-    userEmail = data.get('email', '')
-    examYear = data.get('test_year', '')
-    examSeasonOrMonth = data.get('test_seasonOrMonth', '')
-    orderList = data.get('order_list', [])
-    encoded_image = data.get('image', '')
+    userEmail = request.json['user_email']
+    examYear = request.json['test_year']
+    examSeasonOrMonth = request.json['test_season_or_month']
+    orderList = request.json['order_list']
+    encoded_image = request.json['image']
     try:
         orderedAnswers = utlis.MajorFunction(encoded_image,orderList)
         DB_Management.insertSimulation(userEmail, examYear, examSeasonOrMonth, orderedAnswers)
         resultJson = DB_Management.getSimulationReport(userEmail,examYear,examSeasonOrMonth)
-        return jsonify(resultJson), 200
+        return jsonify(resultJson)
     except Exception as e:
         return jsonify({'error': {'code': 400, 'message': str(e)}}), 400
 
-@app.route('/createUser', methods=['POST'])
+
+
+
+
+
+@app.route('/insertNewUser', methods=['POST'])
 def create_user():
-    data = request.get_json()
-    userEmail = data.get('email', '')
-    userName = data.get('userName', '')
-    password = data.get('password', '')
+    userEmail = request.json['user_email']
+    userName = request.json['user_name']
+    password = request.json['password']
     try:
-         DB_Management.insertUser(userEmail,userName,password)
+        DB_Management.insertUser(userEmail, userName, password)
     except Exception as e:
         return jsonify({'error': {'code': 400, 'message': str(e)}}), 400
     else:
